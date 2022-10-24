84b1fa2591e4c72da50722e22e3a777dfb89805b
Marcelo Yévenes <marcelo.yevenes@inacapmail.cl>
2022-10-24 16:09:56 -0300

    clase 24/10/22

--- nxshitoxApp/models.py
+++ nxshitoxApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+    rut=models.CharField(primary_key=True, max_length=12)
+    nombre=models.TextField(max_length=255)
+    edad=models.IntegerField(null=True)
+    genero=models.CharField(max_length=1)
